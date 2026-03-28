9b9c096be6241ee1795e24b0f2a8dbb571107403
x <mikanabad143@gmail.com>
2026-03-28 11:00:50 +0800

    first commit, gi try ra nako ha

--- src/LogIn.java
+++ src/LogIn.java
@@ -1,5 +1,19 @@
 public class LogIn {
 
     String idNumber;
-    String password;
+    String pass;
+
+    LogIn (String idNumber, String pass){
+        idNumber = idNumber;
+        pass = pass;
+    }
+
+    public String getIdNumber() {
+        return idNumber;
+    }
+
+    public String getPassword() {
+        return pass;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     static void main(String[] args) {
         System.out.println("Hello");
+
+        LogIn l1 = new LogIn(2025-1111, dvvscs);
     }
 }
