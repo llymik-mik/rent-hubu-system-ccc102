f2ecd2ba9909a7fe439ffea579369cb03ddde9cb
x <mikanabad143@gmail.com>
2026-03-28 12:30:14 +0800

    user pani, wako kabalos sunod haha

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
 public class Main {
     static void main(String[] args) {
-        System.out.println("Hello");
-
-        LogIn l1 = new LogIn(2025-1111, );
     }
 }
---
+++ src/User.java
@@ -0,0 +1,15 @@
+public class User {
+
+    String fullName;
+    int idNumber;
+    String password;
+    int contactNum;
+
+    User (String fullName, int idNumber, String password, int contactNum){
+        this.fullName = fullName;
+        this.idNumber = idNumber;
+        this.password = password;
+        this.contactNum = contactNum;
+    }
+
+}
